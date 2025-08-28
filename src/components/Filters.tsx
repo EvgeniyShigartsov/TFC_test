d3ee8286e71d0641fb13a9e6e61e635810c9a02d
Yevhenii Shchyhartsov <y.shchyhartsov@steel.mil.ua>
2025-08-28 21:30:28 +0300

    Add filter optimization

--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -9,15 +9,39 @@ import {
   Slider,
 } from "@mui/material";
 import { observer } from "mobx-react-lite";
+import { useCallback, useMemo, useState } from "react";
 import { useStore } from "~/providers/store/useStore";
+import _ from "lodash";
 
 const genderOptions: Gender[] = ["Female", "Male", "Fluid", "Other"];
 
 export const Filters = observer(() => {
   const store = useStore();
 
-  const ageFrom = store.filters?.age?.min ?? store.userMinMaxAge.min;
-  const ageTo = store.filters?.age?.max ?? store.userMinMaxAge.max;
+  const [localFilters, setLocalFilters] = useState<UserFilters>({
+    age: {
+      min: store.filters?.age?.min ?? store.userMinMaxAge.min,
+      max: store.filters?.age?.max ?? store.userMinMaxAge.max,
+    },
+    gender: store.filters?.gender,
+    textField: store.filters?.textField,
+  });
+
+  const debouncedStoreFilterUpdate = useMemo(
+    () => _.debounce(store.updateFilters, 300),
+    [store]
+  );
+
+  const handleUpdateFilters = useCallback(
+    (field: keyof UserFilters, value: UserFilters[keyof UserFilters]) => {
+      setLocalFilters((p) => ({ ...p, [field]: value }));
+      debouncedStoreFilterUpdate(field, value);
+    },
+    [debouncedStoreFilterUpdate]
+  );
+
+  const ageFrom = localFilters.age!.min!;
+  const ageTo = localFilters.age!.max!;
 
   return (
     <Box
@@ -33,7 +57,7 @@ export const Filters = observer(() => {
         </Typography>
         <Slider
           value={[ageFrom, ageTo]}
-          onChange={(_, [min, max]) => store.updateFilters("age", { min, max })}
+          onChange={(_, [min, max]) => handleUpdateFilters("age", { min, max })}
           min={store.userMinMaxAge.min}
           max={store.userMinMaxAge.max}
         />
@@ -44,7 +68,7 @@ export const Filters = observer(() => {
         <Select
           label="Gender"
           defaultValue=""
-          onChange={(e) => store.updateFilters("gender", e.target.value)}
+          onChange={(e) => handleUpdateFilters("gender", e.target.value)}
         >
           {genderOptions.map((gender) => (
             <MenuItem value={gender} key={gender}>
@@ -55,7 +79,7 @@ export const Filters = observer(() => {
       </FormControl>
 
       <TextField
-        onChange={(e) => store.updateFilters("textField", e.target.value)}
+        onChange={(e) => handleUpdateFilters("textField", e.target.value)}
         label="Search in card"
         type="text"
         size="small"
